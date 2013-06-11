259a8d78b93c4e3c80d063ae3cc7e34b02171501
James Foster <github@jgfoster.net>
2013-06-10 22:11:48 -0700

    Update copyright.

--- GemStone/StartNetLDI.m
+++ GemStone/StartNetLDI.m
@@ -3,7 +3,7 @@
 //  GemStone
 //
 //  Created by James Foster on 7/9/12.
-//  Copyright (c) 2012 VMware Inc. All rights reserved.
+//  Copyright (c) 2012-2013 GemTalk Systems LLC. All rights reserved.
 //
 
 #include <sys/socket.h>
